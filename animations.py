f7a2b51a70eebfb51948e73846c52fd98d8b4a64
Will WS <willwallsoto@gmail.com>
2021-10-09 03:16:43 -0400

    Comments and Formatting

--- animations.py
+++ animations.py
@@ -4,62 +4,115 @@ import random
 from screeninfo import get_monitors
 from itertools import repeat
 
+
 class Canvas:
- window: tk.Tk
- label: tk.Label
- resolution: any
- def __init__(self, window, label, resolution):
-  self.window = window
-  self.label = label
-  self.resolution = resolution
+    """Represents information on the tkinter window and label as well as information of the
+    desktop's width and height, aka resolution
+    """
 
+    window: tk.Tk
+    label: tk.Label
+    resolution: any
 
+    def __init__(self, window, label, resolution):
+        """
+        Args:
+            window (tkinter.Tk)
+            label (tkinter.Label) 
+            resolution (dict[str, int]): must have "width" and "height" as keys
+        """
+        self.window = window
+        self.label = label
+        self.resolution = resolution
+
+    def __repr__(self):
+        return f"<Canvas:c width {self.resolution['width']}px and height {self.resolution['height']}px>"
 
 
 class AnimationStates(Enum):
- IDLE = 0
- IDLE_TO_SLEEP = 1
- SLEEP = 2
- SLEEP_TO_IDLE = 3
- WALK_NEGATIVE = 4
- WALK_POSITIVE = 5
- GRABBED = 6
- FALLING = 7
- LANDED = 8
-
+    """Represents all the possible animation states for our desktop pet
+    """
+    IDLE = 0
+    IDLE_TO_SLEEP = 1
+    SLEEP = 2
+    SLEEP_TO_IDLE = 3
+    WALK_NEGATIVE = 4
+    WALK_POSITIVE = 5
+    GRABBED = 6
+    FALLING = 7
+    LANDED = 8
 
 
 class Animation:
- """Defines the event numbers for this animation 
- and the ways this animation can transfer to the
- next animation
- """
- next_animation_states: list[AnimationStates]
- frames: list 
- frame_timer: int
- v_x: int
- v_y: int
- repititions: int # How many times to repeat given animation before moving onto the next animation
- frame_multiplier: int # Whether or not to duplicate frames (useful for fast updating animations)
+    """Defines the event numbers for this animation
+    and the ways this animation can transfer to the
+    next animation
+    """
 
- def __init__(self, next_animation_states, frames, frame_timer = 100, v_x = 0, v_y = 0, repititions = 0, frame_multiplier = 1):
-    self.next_animation_states = next_animation_states
-    self.frame_timer = frame_timer
-    self.frames = [x for item in frames for x in repeat(item, frame_multiplier)]
-    self.v_x = v_x
-    self.v_y = v_y
-    self.repititions = repititions
+    next_animation_states: list[AnimationStates]
+    frames: list
+    frame_timer: int
+    v_x: int
+    v_y: int
+    repititions: int  # How many times to repeat given animation before moving onto the next animation
+    frame_multiplier: int  # Whether or not to duplicate frames (useful for fast updating animations)
 
- def next(self, animator):
-    if animator.repititions < self.repititions:
-        animator.repititions += 1
-        return animator.state
-    else:
-        animator.repititions = 0 
-    return random.choice(self.next_animation_states)
+    def __init__(
+        self,
+        next_animation_states,
+        frames,
+        frame_timer=100,
+        v_x=0,
+        v_y=0,
+        repititions=0,
+        frame_multiplier=1,
+    ):
+        """
+        Args:
+            next_animation_states (list[Animations]): possible animations for this animation
+            to transition to once this animation finishes
+            frames (list[tk.PhotoImage]): frames of images that can be rendered by tkinter
+            frame_timer (int, optional): time in between every frame of the animation. Defaults to 100.
+            v_x (int, optional): change in x for every frame of the animation. Defaults to 0.
+            v_y (int, optional): change in y for every frame of the animation. Defaults to 0.
+            repititions (int, optional): how many times this animation should repeat before transitioning to the next animation. Defaults to 0.
+            frame_multiplier (int, optional): how many times to duplicate frames in the frames array. This is useful for really
+            fast animations (ie have low frame_timer) to keep the animation updating position but not spazzing out the sprite. Defaults to 1.
+        """
+        self.next_animation_states = next_animation_states
+        self.frame_timer = frame_timer
+        self.frames = [x for item in frames for x in repeat(item, frame_multiplier)]
+        self.v_x = v_x
+        self.v_y = v_y
+        self.repititions = repititions
 
- def get_velocity(self):
-    return (self.v_x, self.v_y)
+    def next(self, animator) -> AnimationStates:
+        """Provides the next animation after this animation finishes. If there are repitions, then it will repeat until 
+        all the repitions are complete before transitionaing
+
+        Args:
+            animator (Animator)
+
+        Returns:
+            AnimationStates
+        """
+        if animator.repititions < self.repititions:
+            animator.repititions += 1
+            return animator.state
+        else:
+            animator.repititions = 0
+        return random.choice(self.next_animation_states)
+
+    def get_velocity(self) -> tuple[int, int]:
+        """returns the change in position that this animation expects
+
+        Returns:
+            tuple[int, int]: change in x, change in y
+        """
+        return (self.v_x, self.v_y)
+    
+    def __repr__(self):
+        return f"<Animation: {len(self.frames)} frames each at {self.frame_timer} ms>"
 
 
 class Animator:
@@ -74,63 +127,96 @@ class Animator:
         self.animations = animations
         self.repititions = 0
 
-    def set_state(self, state):
-        if (state == self.state):
+    def set_state(self, state: AnimationStates) -> None:
+        """Update the state and reset variables that change with each animation
+
+        Args:
+            state (AnimationStates): state to set
+        """
+        if state == self.state:
             return
         self.frame_number = 0
         self.state = state
-
-
-def get_animations(impath):
-    idle = [tk.PhotoImage(file=impath+'idle.gif',format = 'gif -index %i' %(i)) for i in range(5)]#idle gif
-    idle_to_sleep = [tk.PhotoImage(file=impath+'idle_to_sleep.gif',format = 'gif -index %i' %(i)) for i in range(8)]#idle to sleep gif
-    sleep = [tk.PhotoImage(file=impath+'sleep.gif',format = 'gif -index %i' %(i)) for i in range(3)]#sleep gif
-    sleep_to_idle = [tk.PhotoImage(file=impath+'sleep_to_idle.gif',format = 'gif -index %i' %(i)) for i in range(8)]#sleep to idle gif
-    walk_positive = [tk.PhotoImage(file=impath+'walking_positive.gif',format = 'gif -index %i' %(i)) for i in range(8)]#walk to left gif
-    walk_negative = [tk.PhotoImage(file=impath+'walking_negative.gif',format = 'gif -index %i' %(i)) for i in range(8)]#walk to right gif
-
-
-
     
+    def __repr__(self):
+        return f"<Animator: {str(self.state)} on frame {self.frame_number}>"
+
+
+def get_animations(impath:str) -> dict[AnimationStates, Animation]:
+    """Loads all of the animations and their source files into a dictionary
+
+    Args:
+        impath (str): location of all of the animations
+
+    Returns:
+        dict[AnimationStates, Animation]
+    """
+    idle = [
+        tk.PhotoImage(file=impath + "idle.gif", format="gif -index %i" % (i))
+        for i in range(5)
+    ]  # idle gif
+    idle_to_sleep = [
+        tk.PhotoImage(file=impath + "idle_to_sleep.gif", format="gif -index %i" % (i))
+        for i in range(8)
+    ]  # idle to sleep gif
+    sleep = [
+        tk.PhotoImage(file=impath + "sleep.gif", format="gif -index %i" % (i))
+        for i in range(3)
+    ]  # sleep gif
+    sleep_to_idle = [
+        tk.PhotoImage(file=impath + "sleep_to_idle.gif", format="gif -index %i" % (i))
+        for i in range(8)
+    ]  # sleep to idle gif
+    walk_positive = [
+        tk.PhotoImage(
+            file=impath + "walking_positive.gif", format="gif -index %i" % (i)
+        )
+        for i in range(8)
+    ]  # walk to left gif
+    walk_negative = [
+        tk.PhotoImage(
+            file=impath + "walking_negative.gif", format="gif -index %i" % (i)
+        )
+        for i in range(8)
+    ]  # walk to right gif
 
     standing_actions = [AnimationStates.IDLE_TO_SLEEP]
-    #make it less likely to sleep!
+    # make it less likely to sleep!
     standing_actions.extend(repeat(AnimationStates.IDLE, 3))
     standing_actions.extend(repeat(AnimationStates.WALK_NEGATIVE, 4))
     standing_actions.extend(repeat(AnimationStates.WALK_POSITIVE, 4))
 
     ANIMATIONS: dict[AnimationStates, Animation] = {
-    AnimationStates.IDLE: Animation(
-        standing_actions, 
-        idle, frame_timer = 400),   
-    AnimationStates.IDLE_TO_SLEEP: Animation(
-        [AnimationStates.SLEEP], 
-        idle_to_sleep),
-    AnimationStates.SLEEP: Animation(
-        [AnimationStates.SLEEP, AnimationStates.SLEEP, AnimationStates.SLEEP, AnimationStates.SLEEP, AnimationStates.SLEEP_TO_IDLE], 
-        sleep, frame_timer = 1000),
-    AnimationStates.SLEEP_TO_IDLE: Animation(
-        [AnimationStates.IDLE], 
-        sleep_to_idle),
-    AnimationStates.WALK_POSITIVE: Animation(
-        standing_actions, 
-        walk_positive, v_x=3),
-    AnimationStates.WALK_NEGATIVE: Animation(
-        standing_actions, 
-        walk_negative, v_x=-3),
-    AnimationStates.GRABBED:  Animation(
-        [AnimationStates.GRABBED], 
-        walk_positive, frame_timer=50),
-    AnimationStates.FALLING:  Animation(
-        [AnimationStates.FALLING], 
-        walk_negative, frame_timer=10,
-        frame_multiplier = 2),
+        AnimationStates.IDLE: Animation(standing_actions, idle, frame_timer=400),
+        AnimationStates.IDLE_TO_SLEEP: Animation(
+            [AnimationStates.SLEEP], idle_to_sleep
+        ),
+        AnimationStates.SLEEP: Animation(
+            [
+                AnimationStates.SLEEP,
+                AnimationStates.SLEEP,
+                AnimationStates.SLEEP,
+                AnimationStates.SLEEP,
+                AnimationStates.SLEEP_TO_IDLE,
+            ],
+            sleep,
+            frame_timer=1000,
+        ),
+        AnimationStates.SLEEP_TO_IDLE: Animation([AnimationStates.IDLE], sleep_to_idle),
+        AnimationStates.WALK_POSITIVE: Animation(
+            standing_actions, walk_positive, v_x=3
+        ),
+        AnimationStates.WALK_NEGATIVE: Animation(
+            standing_actions, walk_negative, v_x=-3
+        ),
+        AnimationStates.GRABBED: Animation(
+            [AnimationStates.GRABBED], walk_positive, frame_timer=50
+        ),
+        AnimationStates.FALLING: Animation(
+            [AnimationStates.FALLING], walk_negative, frame_timer=10, frame_multiplier=2
+        ),
     }
     ANIMATIONS[AnimationStates.LANDED] = ANIMATIONS[AnimationStates.IDLE]
 
-    
-    assert(len(AnimationStates) == len(ANIMATIONS.keys()))
+    assert len(AnimationStates) == len(ANIMATIONS.keys())
     return ANIMATIONS
-
-
-
--- pets.py
+++ pets.py
@@ -1,102 +1,131 @@
 import random
 import tkinter as tk
-from animations import AnimationStates, Canvas, Animator
+from animations import Animation, AnimationStates, Canvas, Animator
 
 
 class Pet:
- size = 100
- x: int
- y: int
- v_y: float = 0
- a_y: float = .3
- canvas: Canvas
- animator: Animator
+    """Represents a Virtual Desktop Pet that has animations, basic physics, can be picked up, and will stay on screen
+    """
+    size = 100
+    x: int
+    y: int
+    v_y: float = 0
+    a_y: float = 0.3
+    canvas: Canvas
+    animator: Animator
 
- def __init__(self, x, y, canvas, animator):
-  self.x = x
-  self.y = y
-  self.canvas = canvas
-  self.animator = animator 
- 
- def do_movement(self):
-  if (self.x < 0):
-      self.x = Pet.size
-  if self.x > self.canvas.resolution["width"] - Pet.size:
-      self.x = self.canvas.resolution["width"] -  Pet.size
+    def __init__(self, x, y, canvas, animator):
+        self.x = x
+        self.y = y
+        self.canvas = canvas
+        self.animator = animator
 
-  # do stuff with the y position
-  if self.y > self.canvas.resolution["height"]:
-      self.y = self.canvas.resolution["height"]
-      if self.animator.state == AnimationStates.FALLING:
-          self.v_y = 0
-          self.animator.set_state(AnimationStates.LANDED)
-          
-  if self.animator.state == AnimationStates.FALLING:
-      self.v_y += self.a_y
-      self.y += self.v_y
-      self.y = int(self.y)
+    def do_movement(self):
+        """Keep the pet on the screen and if the pet is in the air, then make the pet fall down to the "floor"
+        """
+        if self.x < 0:
+            self.x = Pet.size
+        if self.x > self.canvas.resolution["width"] - Pet.size:
+            self.x = self.canvas.resolution["width"] - Pet.size
 
- def update(self):
-  animation = self.get_current_animation()
-  x, y = animation.get_velocity()
-  self.x += x
-  self.y += y  
-  self.do_movement()
-  self.progress_animation()
+        # do stuff with the y position
+        if self.y > self.canvas.resolution["height"]:
+            self.y = self.canvas.resolution["height"]
+            if self.animator.state == AnimationStates.FALLING:
+                self.v_y = 0
+                self.animator.set_state(AnimationStates.LANDED)
 
- def draw(self):
-  animation = self.get_current_animation()
-  return animation.frames[self.animator.frame_number]
+        if self.animator.state == AnimationStates.FALLING:
+            self.v_y += self.a_y
+            self.y += self.v_y
+            self.y = int(self.y)
 
- def on_tick(self):
-  """Draw the current animation
-  """
-  self.update()
-  frame = self.draw()
-  self.set_geometry()
-  self.canvas.label.configure(image=frame)
-  self.canvas.window.after(1,self.handle_event)
+    def update(self):
+        """Move the pet according to the animation and physics as well as progressing to the next frame of the animation
+        """
+        animation = self.get_current_animation()
+        x, y = animation.get_velocity()
+        self.x += x
+        self.y += y
+        self.do_movement()
+        self.progress_animation()
 
- #transfer random no. to event
- def handle_event(self):
-  """Handle a change in the event_number variable
-  and then apply the appropiate animation state
+    def draw(self) -> tk.PhotoImage:
+        """Ussing the 
 
-  Raises:
-      Exception: [description]
-  """
-  self.canvas.window.after(self.animator.animations[self.animator.state].frame_timer,self.on_tick)
+        Returns:
+            tk.PhotoImage: image of animation to draw
+        """
+        animation = self.get_current_animation()
+        return animation.frames[self.animator.frame_number]
 
-  
- #making gif work 
- def progress_animation(self):
-  animation = self.get_current_animation()
-  if self.animator.frame_number < len(animation.frames) -1:
-   self.animator.frame_number+=1
-  else:
-   self.animator.frame_number = 0
-   self.animator.set_state(animation.next(self.animator))
-#   print(self.animator.state, self.animator.frame_number)
+    def on_tick(self):
+        """Draw the current animation"""
+        self.update()
+        frame = self.draw()
+        self.set_geometry()
+        self.canvas.label.configure(image=frame)
+        self.canvas.window.after(1, self.handle_event)
 
- def get_current_animation(self):
-   return self.animator.animations[self.animator.state]
+    # transfer random no. to event
+    def handle_event(self):
+        """Part of animation loop, after delay between frames in animation
+        proceed to begin logic of drawing next frame
 
- def start_move(self, event):
-      self.animator.set_state(AnimationStates.GRABBED)
-    #   self.x = event.x_root
-    #   self.y = event.y_root
-      self.v_y = 0
+        Raises:
+            Exception: [description]
+        """
+        self.canvas.window.after(
+            self.animator.animations[self.animator.state].frame_timer, self.on_tick
+        )
 
- def stop_move(self, event):
-      self.animator.set_state(AnimationStates.FALLING)
+    # making gif work
+    def progress_animation(self):
+        """Move the animation forward one frame. If the animation has finished (ie current frame is
+        the last frame) then try to progress to the next animation
+        """
+        animation = self.get_current_animation()
+        if self.animator.frame_number < len(animation.frames) - 1:
+            self.animator.frame_number += 1
+        else:
+            self.animator.frame_number = 0
+            self.animator.set_state(animation.next(self.animator))
 
- def do_move(self, event):
-    self.progress_animation()
-    self.x = event.x_root
-    self.y = event.y_root
-    self.v_y = 0
-    self.set_geometry()
-    ## self.geometry(f"+{x}+{y}")
+    #   print(self.animator.state, self.animator.frame_number)
 
- def set_geometry(self):
-   self.canvas.window.geometry(str(Pet.size) + 'x' + str(Pet.size) + '+'+str(self.x)+'+'+str(self.y))
+    def get_current_animation(self) -> Animation:
+        """Returns the current animation of the Pet instance
+
+        Returns:
+            Animation
+        """
+        return self.animator.animations[self.animator.state]
+
+
+
+    def set_geometry(self):
+        """Update the window position and scale to match that of the pet instance's location and size
+        """
+        self.canvas.window.geometry(
+            str(Pet.size) + "x" + str(Pet.size) + "+" + str(self.x) + "+" + str(self.y)
+        )
+    
+    def __repr__(self):
+        return f"<VirtualPet of {Pet.size}x{Pet.size} at ({self.x}, {self.y}) using {str(self.animator)} and {str(self.canvas)}>"
+
+    #################################################### Event Handlers
+    def start_move(self, event):
+        """ Mouse 1 click """
+        self.animator.set_state(AnimationStates.GRABBED)
+        self.v_y = 0
+    def stop_move(self, event):
+        """ Mouse 1 release """
+        self.animator.set_state(AnimationStates.FALLING)
+
+    def do_move(self, event):
+        """ Mouse movement while clicked """
+        self.progress_animation()
+        self.x = event.x_root
+        self.y = event.y_root
+        self.v_y = 0
+        self.set_geometry()
