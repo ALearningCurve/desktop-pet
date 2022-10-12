6ca8c43574f3aae1a844dca04901e1062dcca32d
Will WS <willwallsoto@gmail.com>
2022-10-12 13:01:17 -0400

    formatting

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,11 @@
 # For the module to work
 import logging
 
-logging.basicConfig(format="%(name)s - %(levelname)s: %(message)s",level=logging.INFO)
+logging.basicConfig(format="%(name)s - %(levelname)s: %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-f_handler = logging.FileHandler('logs.log')
+f_handler = logging.FileHandler("logs.log")
 f_handler.setLevel(logging.ERROR)
-f_format = logging.Formatter('%(asctime)s: %(name)s - %(levelname)s: %(message)s')
+f_format = logging.Formatter("%(asctime)s: %(name)s - %(levelname)s: %(message)s")
 f_handler.setFormatter(f_format)
 logger.addHandler(f_handler)
